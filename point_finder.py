3a361c689a6371d01e37266c643f0de69cef22ce
Cole Urwongse <cole.urwongse@gmail.com>
2026-02-23 10:18:04 -0500

    Add files via upload

---
+++ point_finder.py
@@ -0,0 +1,6 @@
+# Useful for finding points of specific indexes in the map file
+num1 = int(input('Column: '))
+num2 = int(input('Row: '))
+num1 *= int(input('Length of row: '))
+num1 += num2 + 1
+print(num1)
